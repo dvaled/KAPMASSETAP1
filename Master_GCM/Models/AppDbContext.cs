4d672d80cf68ba321e64710b31f3abcae083f461
dvaled <davaalif0@gmail.com>
2024-09-24 10:32:43 +0700

    :wrench: add database User, and set NIPPM as Prim key

--- Master_GCM/Models/AppDbContext.cs
+++ Master_GCM/Models/AppDbContext.cs
@@ -6,8 +6,11 @@ public class AppDbContext : DbContext
 
     public DbSet<Master> Masters { get; set; }
 
+    public DbSet<User> Users { get; set; }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Master>().HasKey(e => e.MasterID);
+        modelBuilder.Entity<User>().HasKey(e => e.NIPPM);
     }
 }
