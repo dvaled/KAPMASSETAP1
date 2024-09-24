cfe62153cc09ed81c9c09d7e5a7b0f30a46a9fac
Kevin <khalfanifadillah.com>
2024-09-24 15:46:01 +0700

    add hardware model

---
+++ Master_GCM/Models/Hardware.cs
@@ -0,0 +1,28 @@
+public class hardware{
+    public required string ProcessorBrand{ get; set; }
+    public required string ProcessorModel { get; set; }
+    public required string ProcessorSeries{get; set;}
+    public required string MemoryType{get; set;}
+    public required string MemoryBrand{get; set;}
+    public required string MemoryModel{get; set;}
+    public required string MemorySeries{get; set;}
+    public required string MemoryCapacity{get; set;}
+    public required string StorageType{get; set;}
+    public required string StorageBrand{get; set;}
+    public required string StorageModel{get; set;}
+    public required string StorageCapacity{get; set;}
+    public required string GraphicsType{get; set;}
+    public required string GraphicsBrand{get; set;}
+    public required string GraphicsModel{get; set;}
+    public required string GraphicsSeries{get; set;}
+    public required string GraphicsCapacity{get; set;}
+    public required string ScreenResolution{get; set;}
+    public required bool Touchscreen {get; set;}
+    public required bool BacklightKeyboard{get; set;}
+    public required bool Convertible{get; set;}
+    public required bool Webcamera {get; set;}
+    public required bool Speaker {get; set;}
+    public required bool Microphone {get; set;}
+    public required bool Wifi {get; set;}
+    public required bool Bluetooth {get; set;}
+}
