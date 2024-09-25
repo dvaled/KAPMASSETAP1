cb5c0bbd800b3c0ab5da0a035720cc8e93f7adf8
dvaled <davaalif0@gmail.com>
2024-09-25 08:28:43 +0700

    system history

--- Master_GCM/obj/Debug/net8.0/Master_GCM.AssemblyInfo.cs
+++ Master_GCM/obj/Debug/net8.0/Master_GCM.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Master_GCM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62503aff408648f805340aa931a3240f9cdad934")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f8efa627768ac42711db9aa0bc4f07b6bb70a04")]
 [assembly: System.Reflection.AssemblyProductAttribute("Master_GCM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Master_GCM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
