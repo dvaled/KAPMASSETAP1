83877871bd20f2265029cd517c449fdf6e490e2c
dvaled <davaalif0@gmail.com>
2024-10-04 08:10:39 +0700

    Api Change

--- Master_GCM/obj/Debug/net8.0/Master_GCM.AssemblyInfo.cs
+++ Master_GCM/obj/Debug/net8.0/Master_GCM.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Master_GCM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5aa27054acd3c51f2fc6595d56c3d54e8bfdb1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+032df0dd79706ca38bd3cae804c6627307a3617c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Master_GCM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Master_GCM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
